ef936d4f94c9fbf4764d9386e2c087294823877f
Li-Erh Chang <lechang@yahoo-inc.com>
2015-08-17 15:02:36 +0800

    Initial Commit

---
+++ RottenTomato/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenTomato
+//
+//  Created by Li-Erh å¼µåŠ›å…’ Chang on 8/17/15.
+//  Copyright (c) 2015 Li-Erh Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RottenTomato/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RottenTomato
+//
+//  Created by Li-Erh å¼µåŠ›å…’ Chang on 8/17/15.
+//  Copyright (c) 2015 Li-Erh Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
